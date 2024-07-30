4aae9e2dff89d6465a8dc54c60149de1f70ebbdb
skvar <skvar@SK_Varma.guest.uc.edu>
2024-07-30 18:14:51 -0400

    Added CustomQueryMethods in CRUD Repo

--- demo/src/main/java/com/example/demo/entity/Employee.java
+++ demo/src/main/java/com/example/demo/entity/Employee.java
@@ -6,17 +6,30 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "emp")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
-    @Id
+ 
+	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "emp_name")
     private String name;
+    
+    private String department;
+    
+    private Long salary;
 
     // Getters and Setters
     public Long getId() {
@@ -34,4 +47,20 @@ public class Employee {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public String getDepartment() {
+  		return department;
+  	}
+
+  	public void setDepartment(String department) {
+  		this.department = department;
+  	}
+
+  	public Long getSalary() {
+  		return salary;
+  	}
+
+  	public void setSalary(Long salary) {
+  		this.salary = salary;
+  	}
 }
